7ddfff92fc8e695b8740eec7e0f2154e976350b0
vpletea <84437690+vpletea@users.noreply.github.com>
2024-07-03 09:12:02 +0300

    Update configuration.nix

--- nix-os/configuration.nix
+++ nix-os/configuration.nix
@@ -267,22 +267,22 @@
       settings = {
         PLATFORM_PROFILE_ON_AC = "balanced";
         PLATFORM_PROFILE_ON_BAT = "low-power";
-        
+
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-	      CPU_BOOST_ON_AC = "1";
+        CPU_BOOST_ON_AC = "1";
         CPU_BOOST_ON_BAT = "0";
 
-	      CPU_HWP_DYN_BOOST_ON_AC = "1";
-	      CPU_HWP_DYN_BOOST_ON_BAT = "0";
+        CPU_HWP_DYN_BOOST_ON_AC = "1";
+        CPU_HWP_DYN_BOOST_ON_BAT = "0";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
-      
+
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 30;
       };
