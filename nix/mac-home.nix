6f69d925453bd32de2772cf7fa90dd92080a2a44
vpletea <84437690+vpletea@users.noreply.github.com>
2024-09-09 14:57:29 +0300

    various edits

--- nix/mac-home.nix
+++ nix/mac-home.nix
@@ -3,14 +3,54 @@
 {
   imports =
   [
-    modules/zsh.nix
-    modules/home.nix
     modules/starship.nix
     modules/kitty.nix
     modules/vscode.nix
     modules/ssh.nix
     modules/git.nix
   ];
+
+  # User settings
+  home = {
+    username = "valentin.pletea";
+    homeDirectory = "/Users/valentin.pletea";
+    shellAliases = {
+      ll = "ls -alh";
+      ls = "ls --color=auto";
+      grep = "grep -n --color";
+      kc = "k3d cluster create -p 80:80@loadbalancer -p 443:443@loadbalancer";
+      kd = "k3d cluster delete";
+      dr = "darwin-rebuild switch";
+      hs = "home-manager switch -b backup";
+    };
+    packages = with pkgs; [
+      ansible
+      htop
+      k3d
+      kubectl
+      kubernetes-helm
+      terraform
+      watch
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    history.extended = true;
+    syntaxHighlighting.enable = true;
+    initExtra = ''
+      autoload -Uz history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^[[A" history-beginning-search-backward-end
+      bindkey "^[[B" history-beginning-search-forward-end
+      ssh-add -q ~/.ssh/github.key
+    '';
+  };
+
   # No need to change the version
   home.stateVersion = "24.05";
 
