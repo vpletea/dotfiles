03ad7b69fa12a959a165d01fa4e38b45e9f3e0e5
vpletea <84437690+vpletea@users.noreply.github.com>
2024-09-11 09:29:59 +0300

    zsh substring search

--- nix/modules/zsh.nix
+++ nix/modules/zsh.nix
@@ -9,12 +9,14 @@
     autosuggestion.enable = true;
     history.extended = true;
     syntaxHighlighting.enable = true;
+    historySubstringSearch.enable = true;
     initExtra = ''
-      autoload -Uz history-search-end
-      zle -N history-beginning-search-backward-end history-search-end
-      zle -N history-beginning-search-forward-end history-search-end
-      bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-      bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+      # autoload -Uz history-search-end
+      # zle -N history-beginning-search-backward-end history-search-end
+      # zle -N history-beginning-search-forward-end history-search-end
+      # bindkey -e
+      # bindkey "^[[A" history-beginning-search-backward-end
+      # bindkey "^[[B" history-beginning-search-forward-end
       ssh-add -q ~/.ssh/github.key
     '';
   };
