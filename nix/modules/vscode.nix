8fa7622a181e73c73a541e8be292c4ffcc5deb69
vpletea <84437690+vpletea@users.noreply.github.com>
2024-09-12 18:06:04 +0300

    vscode settings

--- nix/modules/vscode.nix
+++ nix/modules/vscode.nix
@@ -12,28 +12,41 @@
       hashicorp.terraform
       catppuccin.catppuccin-vsc
       waderyan.gitblame
+      redhat.vscode-yaml
     ];
     userSettings = {
-      "workbench.startupEditor" = "none";
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "git.enableSmartCommit"= true;
-      "git.confirmSync" = false;
-      "git.mergeEditor" = true;
+      "[terraform-vars]" = {
+        "editor.defaultFormatter" = "hashicorp.terraform";
+        "editor.formatOnSave" = true;
+      };
+      "[terraform]" = {
+        "editor.defaultFormatter" = "hashicorp.terraform";
+        "editor.formatOnSave" = true;
+      };
+      "diffEditor.ignoreTrimWhitespace" = false;
+      "editor.bracketPairColorization.enabled" = true;
+      "editor.fontFamily" = "'FiraCode Nerd Font', 'monospace', monospace";
+      "editor.fontSize" = 15;
+      "editor.formatOnSave" = false;
       "editor.formatOnType" = true;
       "editor.inlineSuggest.enabled" = true;
-      "editor.bracketPairColorization.enabled" = true;
       "editor.minimap.enabled" = false;
-      "editor.fontSize" = 15;
-      "editor.fontFamily" = "'FiraCode Nerd Font', 'monospace', monospace";
-      "terminal.integrated.fontSize" = 15;
-      "terminal.integrated.fontFamily" = "'FiraCode Nerd Font', 'monospace', monospace";
-      "telemetry.telemetryLevel" = "off";
-      "update.showReleaseNotes" = false;
+      "editor.tabSize" = 2;
+      "extensions.autoCheckUpdates" = false;
       "extensions.ignoreRecommendations" = true;
-      "files.trimTrailingWhitespace" = true;
-      "files.trimFinalNewlines" = true;
       "files.insertFinalNewline" = false;
-      "diffEditor.ignoreTrimWhitespace" = false;
+      "files.trimFinalNewlines" = true;
+      "files.trimTrailingWhitespace" = true;
+      "git.confirmSync" = false;
+      "git.enableSmartCommit" = true;
+      "git.mergeEditor" = true;
+      "telemetry.telemetryLevel" = "off";
+      "terminal.integrated.fontFamily" = "'FiraCode Nerd Font', 'monospace', monospace";
+      "terminal.integrated.fontSize" = 15;
+      "update.mode" = "none";
+      "update.showReleaseNotes" = false;
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.startupEditor" = "none";
     };
   };
 }
