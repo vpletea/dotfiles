2492620d92c745557b1987b474a39d819d1bdf75
vpletea <84437690+vpletea@users.noreply.github.com>
2024-11-20 16:41:08 +0200

    various edits

--- home/flake.nix
+++ home/flake.nix
@@ -2,22 +2,51 @@
   description = "Home Manager configuration of vali.pletea";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = {  self, nixpkgs, nix-darwin, home-manager, ...  }:
     let
-      system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."vali.pletea" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+    configuration = { pkgs, ... }: {
+    imports =
+    [
+      ../common/aliases.nix
+    ];
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+          pkgs.vscode
+        ];
 
+      nixpkgs.config.allowUnfree = true;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+      programs.zsh.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+    in {
+      darwinConfigurations."Valis-iMac-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration 
+        ];
+     };
+      homeConfigurations."vali.pletea" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-darwin";
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
