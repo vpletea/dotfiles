136fd8cb03bcb692d065e57b90a1d829a28c8025
vpletea <84437690+vpletea@users.noreply.github.com>
2024-11-22 14:37:09 +0200

    various edits

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of vali.pletea";
+  description = "Multi OS Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -11,47 +11,17 @@
 
   outputs = {  self, nixpkgs, nix-darwin, home-manager, ...  }:
   {
-      darwinConfigurations."Valis-iMac-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [
-        {
-          nixpkgs.hostPlatform = "x86_64-darwin";
-          nix.settings.experimental-features = "nix-command flakes";
-          # Set your time zone.
-          time.timeZone = "Europe/Bucharest";
-          nixpkgs.config.allowUnfree = true;
-          environment.pathsToLink = [ "/share/zsh" ];
-          users.users."vali.pletea" = {
-            name = "vali.pletea";
-            home = "/Users/vali.pletea";
-          };
-          home-manager.backupFileExtension = "backup";
-          # nix.configureBuildUsers = true;
-          # nix.useDaemon = true;
-          system.configurationRevision = self.rev or self.dirtyRev or null;  # Auto upgrade nix package and the daemon service.
-          services.nix-daemon.enable = true;
-          # nix.package = pkgs.nix;
-          security.pam.enableSudoTouchIdAuth = true;
-          # Create /etc/zshrc that loads the nix-darwin environment.
-          programs.zsh.enable = true;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          system.stateVersion = 4;
-        }
+    darwinConfigurations."Valis-iMac-Pro" = nix-darwin.lib.darwinSystem {
+    # system.configurationRevision = self.rev or self.dirtyRev or null;
+    modules = [
         ./modules/macos-config.nix
         ./modules/aliases.nix
-				home-manager.darwinModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users."vali.pletea" = import ./modules/macos-home.nix;
-				}
-        ];
-      };
-
-      # homeConfigurations."vali.pletea" = home-manager.lib.homeManagerConfiguration {
-      # pkgs = nixpkgs.legacyPackages."x86_64-darwin";
-      #   # Specify your home configuration modules here, for example,
-      #   # the path to your home.nix.
-      #   modules = [ ./modules/macos-home.nix ];
-      # };
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."vali.pletea" = import ./modules/macos-home.nix;
+        }
+      ];
     };
+  };
 }
--- modules/macos-config.nix
+++ modules/macos-config.nix
@@ -1,6 +1,29 @@
-{ config, inputs, pkgs, lib, unstablePkgs, ... }:
+{ pkgs, lib, inputs, customArgs, ... }:
 
 {
+
+nixpkgs.hostPlatform = "x86_64-darwin";
+          nix.settings.experimental-features = "nix-command flakes";
+          # Set your time zone.
+          time.timeZone = "Europe/Bucharest";
+          nixpkgs.config.allowUnfree = true;
+          environment.pathsToLink = [ "/share/zsh" ];
+          users.users."vali.pletea" = {
+            name = "vali.pletea";
+            home = "/Users/vali.pletea";
+          };
+          home-manager.backupFileExtension = "backup";
+          # nix.configureBuildUsers = true;
+          # nix.useDaemon = true;
+ # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+          security.pam.enableSudoTouchIdAuth = true;
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          programs.zsh.enable = true;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          system.stateVersion = 4;
 
   # Packages installed system wide
   environment.systemPackages = with pkgs; [
