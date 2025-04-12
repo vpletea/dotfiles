91b2baed13a70413a76d25e25f6c7e728bfa33da
vpletea <84437690+vpletea@users.noreply.github.com>
2025-04-12 12:33:37 +0300

    nixos flake input fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {  self, nixpkgs, nix-darwin, home-manager, ...  }@inputs:
+  outputs = {  self, nixpkgs, home-manager, ...  }@inputs:
   let
     macos-username = "valentin.pletea";
     nixos-username = "valentin";
