277071d3d2ca259ede2bbb69a5575c4489ba044d
vpletea <84437690+vpletea@users.noreply.github.com>
2025-04-15 16:37:46 +0300

    fix: remove trailing semicolon in flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -18,7 +18,7 @@
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
-      /etc/nixos/hardware-configuration.nix;
+      /etc/nixos/hardware-configuration.nix
       ./module/host.nix
       {
         # Define user account
