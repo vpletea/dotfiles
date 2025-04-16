485d6a62ca669a7ef820e4135b6bda466f5e3729
vpletea <84437690+vpletea@users.noreply.github.com>
2025-04-16 10:57:39 +0300

    feat: restructure nixos flake

--- nixos/user.nix
+++ nixos/user.nix
@@ -3,14 +3,14 @@
 
     imports =
   [
-    ../../config/aliases.nix
-    ../../config/git.nix
-    ../../config/gnome.nix
-    ../../config/kitty.nix
-    ../../config/ssh.nix
-    ../../config/starship.nix
-    ../../config/vscode.nix
-    ../../config/zsh.nix
+    ../config/aliases.nix
+    ../config/git.nix
+    ../config/gnome.nix
+    ../config/kitty.nix
+    ../config/ssh.nix
+    ../config/starship.nix
+    ../config/vscode.nix
+    ../config/zsh.nix
   ];
 
   # No need to change the version
