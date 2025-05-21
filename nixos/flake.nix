fa388ad7dfcbeaaa9dae845a1070769bccffd995
vpletea <84437690+vpletea@users.noreply.github.com>
2025-05-21 10:48:29 +0300

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
