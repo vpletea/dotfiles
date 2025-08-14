b938e4b90474a4ef02b7722e8819152cc1a8e662
vpletea <84437690+vpletea@users.noreply.github.com>
2025-08-14 11:48:08 +0300

    Only run GNOME config script on Linux

--- home/.chezmoiscripts/linux/run_gnome_config.sh
+++ home/.chezmoiscripts/linux/run_gnome_config.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+{{ if eq .chezmoi.os "linux" -}}
+#!/run/current-system/sw/bin/sh
 /run/current-system/sw/bin/dconf reset -f /
 /run/current-system/sw/bin/dconf load / < $HOME/.config/dconf/dconf.ini
+{{- end }}
