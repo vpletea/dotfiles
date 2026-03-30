14367bc0c981759df86b54a6c7146b511f523552
vpletea <84437690+vpletea@users.noreply.github.com>
2026-03-30 15:24:33 +0300

    Update nixpkgs URL to version 25.11

--- flake/flake.nix
+++ flake/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = inputs @ { nixpkgs, ...}:
